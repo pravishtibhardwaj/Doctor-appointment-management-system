d150ff5c3811e3344e1557468c5d6b87d61f9b53
pravishti bhardwaj <bhard.pravi08@gmail.com>
2023-12-14 23:13:20 +0530

    model created

---
+++ model/model.go
@@ -0,0 +1,25 @@
+package model
+
+import "time"
+type Doctor struct {
+	ID      int    `json:"id"`
+	D_name  string `json:"name"`
+	D_age   int    `json:"age"`
+	D_phone string `json:"class"`
+}
+
+type Patient struct {
+	ID        int    `json:"id"`
+	P_name    string `json:"name"`
+	P_age     int    `json:"age"`
+	P_phone   string `json:"phone"`
+	P_address string `json:"address"`
+}
+
+type Apoointemnt struct {
+	ID   int `json:"id"`
+	Patient_name string `json:"pname"`
+	Doctor_name string `json:"dname"`
+	Apointment_time time.Time `json:"date"`
+	Status string `json:"status"`
+}
